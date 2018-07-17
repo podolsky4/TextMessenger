1d290417c48cfd47da3ef04b6419f1071af4aa60
korsa719 <k.matlayev@gmail.com>
2018-07-17 23:42:58 +0300

    Create ill-database.sql

---
+++ src/main/resources/ill-database.sql
@@ -0,0 +1,7 @@
+insert into users (login, `e-mail`, `registration-date`, password, gender) VALUES ('test','test@gmail.com',CURRENT_TIMESTAMP,'test','Male');
+insert into users (login, `e-mail`, `registration-date`, password, gender) VALUES ('test1','test1@gmail.com',CURRENT_TIMESTAMP,'test1','Female');
+insert into users (login, `e-mail`, `registration-date`, password, gender) VALUES ('test2','test2@gmail.com',CURRENT_TIMESTAMP,'test2','Male');
+insert into users (login, `e-mail`, `registration-date`, password, gender) VALUES ('test3','test3@gmail.com',CURRENT_TIMESTAMP,'test3','Female');
+insert into users (login, `e-mail`, `registration-date`, password, gender) VALUES ('test4','test4@gmail.com',CURRENT_TIMESTAMP,'test4','Male');
+insert into users (login, `e-mail`, `registration-date`, password, gender) VALUES ('test5','test5@gmail.com',CURRENT_TIMESTAMP,'test5','Female');
+insert into users (login, `e-mail`, `registration-date`, password, gender) VALUES ('test6','test6@gmail.com',CURRENT_TIMESTAMP,'test6','Male');
