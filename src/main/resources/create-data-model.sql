c9148cdddfe1daf06660325bb85034af4247eb95
korsa719 <k.matlayev@gmail.com>
2018-07-17 23:42:07 +0300

    Update create-data-model.sql

--- src/main/resources/create-data-model.sql
+++ src/main/resources/create-data-model.sql
@@ -2,12 +2,12 @@ CREATE TABLE users
 (
     id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
     login varchar(50) NOT NULL,
-    `e-mail` VARCHAR(50) NOT NULL,
-    `registration-date` timestamp NOT NULL,
+    `e-mail` varchar(50) NOT NULL,
+    `registration-date` TIMESTAMP NOT NULL,
     password varchar(50) NOT NULL,
-    gender int
+    gender char(6) ,
+    check(gender in ('Male', 'Female'))
 );
-CREATE UNIQUE INDEX users_id_uindex ON users (id);
-CREATE UNIQUE INDEX users_login_uindex ON users (login);
-CREATE UNIQUE INDEX `users_e-mail_uindex` ON users (`e-mail`);
-CREATE UNIQUE INDEX `users_registration-date_uindex` ON users (`registration-date`);
+CREATE UNIQUE INDEX users1_id_uindex ON users1 (id);
+CREATE UNIQUE INDEX users1_login_uindex ON users1 (login);
+CREATE UNIQUE INDEX `users1_e-mail_uindex` ON users1 (`e-mail`)
