f72fa9ae1bcc28088113ce3f66c023ce5aecb16d
full_iron <ilyafefelov@gmail.com>
2018-07-19 18:35:50 +0300

    Es lint (#39)
    * added client-side
    
    * SASS compiler without eject 1
    
    * SASS compiler without eject
    
    * ESlint

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,23 @@ import React, { Component } from 'react'
 import './App.css'
 
 class App extends Component {
-  render() {
-    let Home = () => <div>Hello React!</div>;
-    let HelloRouter = () => <div>Hello React-Router!</div>;
+  render () {
+    let Home = () => <div>Hello React!</div>
+    let HelloRouter = () => <div>Hello React-Router!</div>
     return (
-        <div className="App">
-            with compiler
-          <h1>React Router</h1>
-          <Link to="/">Home</Link>&nbsp;
-          <Link to="/hello">Test Router</Link><hr />
-          <Switch>
-            <Route exact path='/' render={Home} />
-            <Route exact path='/hello' render={HelloRouter} />
-          </Switch>
-        </div>
-    );
+      <div className='App'>
+        with sass-compiler, ESLint
+        <h1>React Router</h1>
+        <Link to='/'>Home</Link>&nbsp;
+        <Link to='/hello'>Test Router</Link>
+        <hr/>
+        <Switch>
+          <Route exact path='/' render={Home}/>
+          <Route exact path='/hello' render={HelloRouter}/>
+        </Switch>
+      </div>
+    )
   }
 }
-export default App;
+
+export default App
