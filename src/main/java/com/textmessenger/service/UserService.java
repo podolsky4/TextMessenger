c31e0a509dee90c0735ec319e3b4ae1e81f4ad68
Igor Rudenko <fac30ff@gmail.com>
2018-07-19 16:42:14 +0300

    added rows between method

--- src/main/java/com/textmessenger/service/UserService.java
+++ src/main/java/com/textmessenger/service/UserService.java
@@ -5,8 +5,11 @@ import com.textmessenger.model.User;
 public interface UserService {
 
   long createUser(User user);
+
   User readUser(long id);
+
   void updateUser(long id, User user);
+
   void deleteUser(long id);
 
 }
