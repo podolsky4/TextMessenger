50efed28870d4739e3960846227449c45c4d74bf
korsa719 <k.matlayev@gmail.com>
2018-07-19 13:03:03 +0300

    create react-router simple template (#28)

--- text-messenger-client/README.md
+++
@@ -1,7 +0,0 @@
-**cd** _text-messenger-client_  
-  
-**to install**  
-`yarn`
-  
-**to start**  
-`yarn start` 
--- text-messenger-client/src/Containers/404/404.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./404.css";
-
-export default () =>
-    <div className="p404">
-        <h3>Sorry, page not found!</h3>
-    </div>;
