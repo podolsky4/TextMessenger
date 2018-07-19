34168f3f3d5687d34763c5cabf9555f0e16d4c0f
Illya “full-iron” Fefelov <ilyafefelov@gmail.com>
2018-07-19 17:58:04 +0300

    ESlint

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,9 @@
-import { BrowserRouter  } from 'react-router-dom';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from 'react-router-dom'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<BrowserRouter><App/></BrowserRouter>, document.getElementById('root'))
+registerServiceWorker()
