b841d4f47cc7495ce44239e3268674fb1e5524f2
Igor Rudenko <32242705+fac30ff@users.noreply.github.com>
2018-07-19 22:18:26 +0300

    add field for User model, and add asome fileds into Message.class (#51)

--- src/main/java/com/textmessenger/model/Message.java
+++ src/main/java/com/textmessenger/model/Message.java
@@ -4,8 +4,11 @@ import lombok.Data;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.Date;
 
 @Entity
 @Table(name = "messages")
@@ -13,6 +16,8 @@ import javax.persistence.Table;
 public class Message {
 
   @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "message_id")
   private long id;
 
   @Column(name = "from_user")
@@ -23,4 +28,7 @@ public class Message {
 
   @Column(name = "message")
   private String message;
+
+  @Column(name = "created_date")
+  private Date date;
 }
--- src/main/java/com/textmessenger/model/User.java
+++ src/main/java/com/textmessenger/model/User.java
@@ -1,4 +1,38 @@
 package com.textmessenger.model;
 
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+@Data
 public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "user_id")
+  private long id;
+
+  @Column(name = "name")
+  private String name;
+
+  @Column(name = "user_name")
+  private String userName;
+
+  @Column(name = "surname")
+  private String surname;
+
+  @Column(name = "email")
+  private String email;
+
+  @Column(name = "password")
+  private char[] password;
+
+
 }
