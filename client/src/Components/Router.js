4ab77417868bdceb467573c8497ddbf8e8a9f01c
korsa719 <k.matlayev@gmail.com>
2018-07-20 13:15:06 +0300

    create 5 links in the header

--- client/src/Components/Router.js
+++ client/src/Components/Router.js
@@ -4,13 +4,19 @@ import React, { Component } from 'react'
 
 class Router extends Component {
   render () {
-    let Home = () => <div>Hello React!</div>
-    let HelloRouter = () => <div>Hello React-Router!</div>
+    let Home = () => <div>Text Messages page</div>;
+    let FeedPage = () => <div>Feed page</div>;
+    let FavoritesPage = () => <div>Favorites page</div>;
+    let MessagesPage = () => <div>Messages page</div>;
+    let NotificationsPage = () => <div>Notifications page</div>;
 
     return (
       <Switch>
         <Route exact path='/' render={Home}/>
-        <Route exact path='/hello' render={HelloRouter}/>
+        <Route exact path='/feed' render={FeedPage}/>
+        <Route exact path='/favorites' render={FavoritesPage}/>
+        <Route exact path='/messages' render={MessagesPage}/>
+        <Route exact path='/notifications' render={NotificationsPage}/>
       </Switch>
     )
   }
