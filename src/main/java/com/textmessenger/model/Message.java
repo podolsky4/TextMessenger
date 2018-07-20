5076e717ed2f882375a4f57876e86af9c60fb429
Igor Rudenko <32242705+fac30ff@users.noreply.github.com>
2018-07-20 11:59:07 +0300

    created crud UserServiceJpa and some fix in Message class and main app class (#52)
    * created crud UserServiceJpa
    
    * add filed private instead of final
    
    * thera too only force push
    
    * rename and remove method @transactional

--- src/main/java/com/textmessenger/model/Message.java
+++ src/main/java/com/textmessenger/model/Message.java
@@ -1,18 +1,24 @@
 package com.textmessenger.model;
 
 import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import java.util.Date;
 
 @Entity
 @Table(name = "messages")
 @Data
+@EntityListeners(AuditingEntityListener.class)
 public class Message {
 
   @Id
@@ -30,5 +36,7 @@ public class Message {
   private String message;
 
   @Column(name = "created_date")
+  @Temporal(TemporalType.TIMESTAMP)
+  @CreatedDate
   private Date date;
 }
