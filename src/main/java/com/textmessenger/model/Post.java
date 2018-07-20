56f69aeea48a75c21bda30010127cad9939f4685
korsa719 <k.matlayev@gmail.com>
2018-07-20 16:28:16 +0300

    fix problems with mapping User&Messages by Igor source code
    added in User field posts
    with annotation @OneToMany
    
    added in Post field user
    with annotation @ManyToOne

--- src/main/java/com/textmessenger/model/Post.java
+++ src/main/java/com/textmessenger/model/Post.java
@@ -2,7 +2,14 @@ package com.textmessenger.model;
 
 import lombok.Data;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.JoinColumn;
 
 @Entity
 @Data
