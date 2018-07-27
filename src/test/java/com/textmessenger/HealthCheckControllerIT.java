14aa4c70bfa703452b00002e181866ade8b76c5e
Igor Rudenko <fac30ff@gmail.com>
2018-07-27 21:52:44 +0300

    add healthcheck controller and 2 test for runtime checking

---
+++ src/test/java/com/textmessenger/HealthCheckControllerIT.java
@@ -0,0 +1,42 @@
+package com.textmessenger;
+
+import java.net.URL;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HealthCheckControllerIT {
+
+  @LocalServerPort
+  private int port;
+
+  private URL base;
+
+  @Autowired
+  private TestRestTemplate template;
+
+  @Before
+  public void setUp() throws Exception {
+    this.base = new URL("http://localhost:" + port + "/healthcheck");
+  }
+
+  @Test
+  public void getHealthCheck() throws Exception {
+    ResponseEntity<String> response = template.getForEntity(base.toString(),
+            String.class);
+    assertThat(response.getBody(), equalTo("live"));
+  }
+}
+
