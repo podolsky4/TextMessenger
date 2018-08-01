7f7afbc4c95b4b41cce1585140b80bafc07505c7
Yuri Podolsky <podolsky4@gmail.com>
2018-08-01 19:12:36 +0300

    Add Material UI React

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,31 +1,168 @@
-import React, {Component} from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import classnames from 'classnames';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import cyan from '@material-ui/core/colors/red';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
-export default class Post extends Component {
-  render () {
+const styles = theme => ({
+  card: {
+    maxWidth: 1200,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  actions: {
+    display: 'flex',
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+    marginLeft: 'auto',
+    [theme.breakpoints.up('sm')]: {
+      marginRight: -8,
+    },
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: cyan[500],
+  },
+});
+
+
+// export default class Post extends Component {
+//   render () {
+//     return (
+//       <div className="post" key={this.props.post.id}>
+//         <header>
+//           <img className="logo" alt="logo" src="https://www.ozilis.com/25038-large_default/plate-42-44-46.jpg" />
+//           <div className="user_info">
+//             <div className="post_login">{this.props.post.user.login}</div>
+//             <div className="post_email">{this.props.post.user.email}</div>
+//           </div>
+//           <div className="data_info">
+//             <div className="user_fullname">{`${this.props.post.user.firstName}  ${this.props.post.user.lastName}`}</div>
+//             <div className="date_created"></div>
+//           </div>
+//         </header>
+//
+//         <p className="post_content">
+//           {this.props.post.content}
+//         </p>
+//         <footer>
+//           <a className="like">Like</a>
+//           <a className="retwite">Retwite</a>
+//           <a className="comment">Comment</a>
+//         </footer>
+//       </div>
+//     )
+//   }
+// }
+
+
+class Post extends React.Component {
+  state = { expanded: false };
+
+  handleExpandClick = () => {
+    this.setState(state => ({ expanded: !state.expanded }));
+  };
+
+  render() {
+    const { classes } = this.props;
+
     return (
-      <div className="post"
-        key={this.props.post.id}>
-        <header>
-          <img className="logo" alt="logo" src="https://www.ozilis.com/25038-large_default/plate-42-44-46.jpg"></img>
-          <div className="user_info">
-            <div className="post_login">{this.props.post.user.login}</div>
-            <div className="post_email">{this.props.post.user.email}</div>
-          </div>
-          <div className="data_info">
-            <div className="user_fullname">{`${this.props.post.user.firstName}  ${this.props.post.user.lastName}`}</div>
-            <div className="date_created">{this.props.post.user.createdDate}</div>
-          </div>
-        </header>
-
-        <p className="post_content">
-          {this.props.post.content}
-        </p>
-        <footer>
-          <a className="like">Like</a>
-          <a className="retwite">Retwite</a>
-          <a className="comment">Comment</a>
-        </footer>
+      <div>
+        <Card className={classes.card}>
+          <CardHeader
+            avatar={
+              <Avatar aria-label="Recipe" className={classes.avatar}>
+                <img className="logo" alt="logo" src="https://www.ozilis.com/25038-large_default/plate-42-44-46.jpg" />
+              </Avatar>
+            }
+            action={
+              <IconButton>
+                <MoreVertIcon />
+              </IconButton>
+            }
+            title={this.props.post.user.login}
+            subheader={this.props.post.user.createdDate}
+          />
+          <CardContent>
+            <Typography component="p">
+              {this.props.post.content}
+            </Typography>
+          </CardContent>
+          <CardActions className={classes.actions} disableActionSpacing>
+            <IconButton aria-label="Add to favorites">
+              <FavoriteIcon />
+            </IconButton>
+            <IconButton aria-label="Share">
+              <ShareIcon />
+            </IconButton>
+            <IconButton
+              className={classnames(classes.expand, {
+                [classes.expandOpen]: this.state.expanded,
+              })}
+              onClick={this.handleExpandClick}
+              aria-expanded={this.state.expanded}
+              aria-label="Show more"
+            >
+              <ExpandMoreIcon />
+            </IconButton>
+          </CardActions>
+          <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
+            <CardContent>
+              <Typography paragraph variant="body2">
+                Method:
+              </Typography>
+              <Typography paragraph>
+                Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
+                minutes.
+              </Typography>
+              <Typography paragraph>
+                Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
+                heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
+                browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving
+                chicken and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion,
+                salt and pepper, and cook, stirring often until thickened and fragrant, about 10
+                minutes. Add saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+              </Typography>
+              <Typography paragraph>
+                Add rice and stir very gently to distribute. Top with artichokes and peppers, and
+                cook without stirring, until most of the liquid is absorbed, 15 to 18 minutes.
+                Reduce heat to medium-low, add reserved shrimp and mussels, tucking them down into
+                the rice, and cook again without stirring, until mussels have opened and rice is
+                just tender, 5 to 7 minutes more. (Discard any mussels that don’t open.)
+              </Typography>
+              <Typography>
+                Set aside off of the heat to let rest for 10 minutes, and then serve.
+              </Typography>
+            </CardContent>
+          </Collapse>
+        </Card>
       </div>
-    )
+    );
   }
 }
+
+Post.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Post);
--- client/src/components/Posts.js
+++ client/src/components/Posts.js
@@ -1,21 +1,41 @@
-import React, {Component} from 'react'
-import {connect} from 'react-redux'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import Post from './Post'
+import { withStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    height: 140,
+    width: 100,
+  },
+  control: {
+    padding: theme.spacing.unit * 2,
+  },
+})
 
 class Posts extends Component {
   render () {
+    const {classes} = this.props
     return (
-      <div className="posts">
-        {this.props.posts.map(content =>
-          <Post
-            key={content.id}
-            post={content}
-          />)}
-      </div>
+      <Grid container className={classes.root}
+            justify="center"
+            direction="column"
+            alignItems="center"
+            spacing={16}>
+        {this.props.posts.map(content => (
+          <Grid item xs={12} lg={12}>
+            <Post key={content.id} post={content}/>
+          </Grid>
+        ))}
+      </Grid>
     )
   }
 }
 
 export default connect(state => ({
   posts: state.posts
-}))(Posts)
+}))(withStyles(styles)(Posts))
