efb3f769013225f0de9df9df3339e80803a9fa7b
IgorRudenko <fac30ff@gmail.com>
2018-08-02 22:58:38 +0300

    add dialog version 0.5

--- client/src/actions/dialogActions.js
+++ client/src/actions/dialogActions.js
@@ -1,7 +1,19 @@
 import {LOAD_DIALOGS} from "./types";
 
 export const loadDialog = id => dispatch => {
-  fetch(`/dialogs/${id}`)
+  fetch(`/api/dialogs/${id}`)
       .then(res => res.json())
       .then(data => dispatch({type:LOAD_DIALOGS, payload:data}))
-};
+}
+
+export const createDialog = id => dispatch => {
+
+}
+
+export const loadMessages = dialog_id => dispatch => {
+
+}
+
+export const createMessage = dialog_id => dispatch => {
+
+}
--- client/src/components/Dialogs.js
+++ client/src/components/Dialogs.js
@@ -1,14 +1,42 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import {loadDialog} from "../actions/dialogActions";
+import {loadDialog} from "../actions/dialogActions"
+import {getUser} from "../actions/userActions";
 
 class Dialog extends Component {
+  componentDidMount(){
+    const {user, dialogs, getUser} = this.props
+    if (user.length === 0) {
+      getUser()
+    }
+    if (dialogs.length === 0) {
+      loadDialog(user.id)
+    }
+  }
 
   render () {
+    const {user, dialogs, getUser} = this.props
     return (
-      <a>Hello from Dialog page</a>
+        dialogs.map(dialog => {
+              <Dialog key = {dialog.id} data = {dialog}/>
+            }
+        )
     )
   }
 }
 
-export default connect()(Dialog)
+const mapStateToProps = state => {
+  return {
+    user: state.user,
+    dialogs: state.dialogs
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    loadDialog: (id) => dispatch(loadDialog(id)),
+    getUser: () => dispatch(getUser())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dialog)
