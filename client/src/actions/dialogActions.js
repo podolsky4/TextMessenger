8e6934e009ec817719d4f7215f292b80e111039d
IgorRudenko <fac30ff@gmail.com>
2018-08-04 00:54:38 +0300

    add some more dialog

--- client/src/actions/dialogActions.js
+++ client/src/actions/dialogActions.js
@@ -1,19 +1,27 @@
 import {LOAD_DIALOGS} from "./types";
 
 export const loadDialog = id => dispatch => {
-  fetch(`/api/dialogs/${id}`)
+  fetch(`/api/dialogs/user/${id}`)
       .then(res => res.json())
       .then(data => dispatch({type:LOAD_DIALOGS, payload:data}))
 }
 
-export const createDialog = id => dispatch => {
-
+export const createDialog = (id, dialog) => dispatch => {
+  fetch(`/api/dialogs/user/${id}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({'dialog': dialog})
+      })
+      .then(() => dispatch(loadDialog(id)))
 }
 
 export const loadMessages = dialog_id => dispatch => {
 
 }
 
-export const createMessage = dialog_id => dispatch => {
+export const createMessage = (dialog_id, message) => dispatch => {
 
 }
