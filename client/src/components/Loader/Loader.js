b4030cf954e4fcad4a7390bacd93c79e7f0726e1
korsa719 <k.matlayev@gmail.com>
2018-08-11 13:16:51 +0300

    create user data in the header create LogIn/LogOut

--- client/src/components/Loader/Loader.js
+++ client/src/components/Loader/Loader.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import CircularProgress  from '@material-ui/core/CircularProgress';
-import green from '@material-ui/core/colors/green';
+import React from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import green from '@material-ui/core/colors/green'
 
 const styles = theme => ({
-    progress: {
-        margin: theme.spacing.unit * 2,
-    },
-});
+  progress: {
+    margin: theme.spacing.unit * 2
+  }
+})
 
-function Loader(props) {
-    const { classes } = props;
-    return (
-        <div>
-            <CircularProgress className={classes.progress} style={{ color: green[900] }} thickness={2} />
-        </div>
-    );
+function Loader (props) {
+  const { classes } = props
+  return (
+    <div>
+      <CircularProgress className={classes.progress} style={{ color: green[900] }} thickness={2} />
+    </div>
+  )
 }
 
 Loader.propTypes = {
-    classes: PropTypes.object.isRequired,
-};
+  classes: PropTypes.object.isRequired
+}
 
-export default withStyles(styles)(Loader);
+export default withStyles(styles)(Loader)
