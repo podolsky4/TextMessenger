168e3bcc1da413f788dfceee7903459ee756d6a1
Illya “full-iron” Fefelov <ilyafefelov@gmail.com>
2018-08-13 15:32:52 +0300

    SignIn Form on HomePage. creates user each time.
    Just visiual representation. Actions are copied from create user component.

--- client/src/views/HomePage/HomePage.js
+++ client/src/views/HomePage/HomePage.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react'
 import Typography from '@material-ui/core/Typography'
+import SignIn from "../../containers/SignIn/SignIn";
 
 class HomePage extends Component {
   render () {
     return (
       <Typography variant='display2' gutterBottom align='center'>
         This page is not implemented yet
+        <SignIn classes={"SignIn"}/>
       </Typography>
     )
   }
