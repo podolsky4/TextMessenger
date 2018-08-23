5f063a2bea57c7697b941b29bb2c40148ffae56b
korsa719 <qq936500merlin>
2018-08-23 21:12:45 +0300

    fix imoport

--- src/main/java/com/textmessenger/controller/UserController.java
+++ src/main/java/com/textmessenger/controller/UserController.java
@@ -4,7 +4,15 @@ import com.textmessenger.model.entity.Post;
 import com.textmessenger.model.entity.User;
 import com.textmessenger.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.lang.reflect.Array;
 import java.util.Optional;
