11de20641867d7a550552dc7115d277c1b2b3654
korsa719 <qq936500merlin>
2018-08-29 22:13:24 +0300

    add dto in Message Controller "MessageFromFront" with 1 field message

---
+++ src/main/java/com/textmessenger/model/entity/dto/MessageFromFront.java
@@ -0,0 +1,11 @@
+package com.textmessenger.model.entity.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class MessageFromFront {
+  @NotBlank
+  private String message;
+}
