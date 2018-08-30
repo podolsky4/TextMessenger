db611a65cd34ba6a5b698f101d2c247a954099de
Illya “full-iron” Fefelov <ilyafefelov@gmail.com>
2018-08-30 20:25:20 +0300

    fixed login render

--- client/src/views/HomePage/HomePage.js
+++ client/src/views/HomePage/HomePage.js
@@ -37,12 +37,16 @@ class HomePage extends Component {
 
   render () {
     const {classes, user} = this.props
+    console.log("user at login: ", user)
     return (
       <div className={classNames(classes.homepage, classes.root, classes.cont)}>
         <TextCard
           text="TextMessenger проект предполагает создание приложения с использованием всех знаний, которые мы получили во время курса. Результатом станет адаптивное веб-приложение, развернутое на AWS."
           classes={'TextCard'}/>
-        {user.length === 0 && <SignIn classes={'SignIn'}/>}
+        {!user.id &&
+          <SignIn classes={'SignIn'}/>
+        }
+
       </div>
     )
   }
