d3e063541a1e0bfc80f1029694b06f56a47c2488
korsa719 <qq936500merlin>
2018-08-30 14:37:48 +0300

    Transform message from front

--- src/main/java/com/textmessenger/controller/MessageController.java
+++ src/main/java/com/textmessenger/controller/MessageController.java
@@ -2,6 +2,7 @@ package com.textmessenger.controller;
 
 import com.textmessenger.model.entity.Dialog;
 import com.textmessenger.model.entity.Message;
+import com.textmessenger.model.entity.dto.MessageFromFront;
 import com.textmessenger.service.MessageService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.Optional;
 
 @RestController
@@ -33,8 +35,8 @@ public class MessageController {
 
   @PostMapping("/user/{userId}/dialog/{id}")
   public ResponseEntity addMessageToDialog(@PathVariable("userId") Long user,
-                                           @PathVariable("id") Long dialog, @RequestBody String msg) {
-    messageService.createMessageWithUserIdDialogId(user, dialog, msg);
+                                           @PathVariable("id") Long dialog, @Valid @RequestBody MessageFromFront msg) {
+    messageService.createMessageWithUserIdDialogId(user, dialog, msg.getMessage());
     return Optional.of(ResponseEntity.ok()).orElse(ResponseEntity.badRequest()).build();
   }
 
