924b79127a9abf0e7544e3b25ef73b6d6d7a7b28
korsa719 <qq936500merlin>
2018-08-31 14:36:39 +0300

    Cover all method test UserRepository

--- src/test/java/com/textmessenger/repository/UserRepositoryTest.java
+++ src/test/java/com/textmessenger/repository/UserRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -30,12 +31,37 @@ public class UserRepositoryTest {
 
   @Test
   public void findUserByLogin() {
+    User user = setUser();
+    User junit = userRepository.findUserByLogin("junit");
+    assertThat(user.equals(junit));
+  }
+
+  @Test
+  public void findUserByEmail() {
+    User user = setUser();
+    User junit = userRepository.findUserByEmail("junit@gmail.com");
+    assertThat(user.equals(junit));
+  }
+
+  @Test
+  public void findUserByEmailOrLogin() {
+    User user = setUser();
+    Optional<User> byLoginOrEmail = userRepository.findByLoginOrEmail("junit", "junit");
+    assertThat(user.equals(byLoginOrEmail.get()));
+  }
+
+  @Test
+  public void findUserByEmailOrLoginSearch() {
+    User user = setUser();
+    List<User> byLoginOrEmail = userRepository.findByEmailContainingIgnoreCaseOrLoginContainingIgnoreCase("junit", "junit");
+    assertThat(user.equals(byLoginOrEmail.get(0)));
+  }
+
+  public User setUser() {
     User user = new User();
     user.setLogin("junit");
     user.setEmail("junit@gmail.com");
     user.setPassword("123456");
-    entityManager.persist(user);
-    User junit = userRepository.findUserByLogin("junit");
-    assertThat(user.equals(junit));
+    return entityManager.persist(user);
   }
 }
