8e23a6bfac2dc1ae04af22f9a1929e2eac27e2fe
korsa719 <qq936500merlin>
2018-09-02 17:19:47 +0300

    EsLint && refactor

--- client/src/components/Post/Post.js
+++ client/src/components/Post/Post.js
@@ -117,7 +117,7 @@ class Post extends Component {
             classes
             post={post}
             currentUser={user}/>
-          <img src={post.imgUrl}/>
+          <img alt="user image" src={post.imgUrl}/>
           <PostContent content={post.content}/>
 
           <Divider/>
