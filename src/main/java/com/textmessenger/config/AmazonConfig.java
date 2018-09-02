0cc7c36434a3620ce403fa6acb682d362cb0f4ca
korsa719 <qq936500merlin>
2018-09-02 11:48:16 +0300

    Access_key Secret_key

--- src/main/java/com/textmessenger/config/AmazonConfig.java
+++ src/main/java/com/textmessenger/config/AmazonConfig.java
@@ -1,9 +1,18 @@
 package com.textmessenger.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @PropertySource("amazon.propetries")
 @Component
 public class AmazonConfig {
+
+  @Value("${Access_key}")
+  private String accessKey;
+
+  @Value("${Secret_key}")
+  private String secretKey;
+
+
 }
