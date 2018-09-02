f40bdff55167236e59e332b39ae3475121f6b75f
korsa719 <qq936500merlin>
2018-09-02 11:10:21 +0300

    pointed out PropertySource amazon.properties

--- src/main/java/com/textmessenger/config/AmazonConfig.java
+++ src/main/java/com/textmessenger/config/AmazonConfig.java
@@ -1,7 +1,9 @@
 package com.textmessenger.config;
 
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
+@PropertySource("amazon.propetries")
 @Component
 public class AmazonConfig {
 }
