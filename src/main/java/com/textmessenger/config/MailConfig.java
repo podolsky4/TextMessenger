42934e4c123a3eb452e3bf46cfb65d7575a03ded
Igor Rudenko <fac30ff@gmail.com>
2018-09-03 16:34:58 +0300

    add general config file with configurations that put in gitignore

--- src/main/java/com/textmessenger/config/MailConfig.java
+++ src/main/java/com/textmessenger/config/MailConfig.java
@@ -10,7 +10,7 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 import java.util.Properties;
 
 @Configuration
-@PropertySource("mail.properties")
+@PropertySource("config.properties")
 public class MailConfig {
 
   @Value("${gmail.userName}")
