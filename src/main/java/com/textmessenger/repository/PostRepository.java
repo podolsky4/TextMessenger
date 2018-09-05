ec4b5a7bd44fe2b8529c53b0c94cd1ee82686659
korsa719 <qq936500merlin>
2018-09-05 16:52:55 +0300

    pageable

--- src/main/java/com/textmessenger/repository/PostRepository.java
+++ src/main/java/com/textmessenger/repository/PostRepository.java
@@ -3,6 +3,8 @@ package com.textmessenger.repository;
 
 import com.textmessenger.model.entity.Post;
 import com.textmessenger.model.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +14,6 @@ import java.util.List;
 public interface PostRepository extends JpaRepository<Post, Long> {
 
   List<Post> findPostsByUser(User user);
+
+  Page<Post> findAllByOrderByCreatedDateDesc(Pageable pageable);
 }
