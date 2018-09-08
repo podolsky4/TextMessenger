d61633b7d7f5a26c33bc671a0b059f1f28dd8e8f
Illya “full-iron” Fefelov <ilyafefelov@gmail.com>
2018-09-08 21:24:44 +0300

    messages design
    Signed-off-by: Illya “full-iron” Fefelov <ilyafefelov@gmail.com>

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types'
 import {withStyles} from '@material-ui/core/styles'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
+import Avatar from '@material-ui/core/Avatar'
 
 // class Message extends Component {
 //   render () {
@@ -30,11 +31,34 @@ const styles = theme => ({
     margin: '.15em',
     width: 'fit-content'
   },
+  messageHeader: {
+    display: 'flex',
+    flexDirection: 'row'
+    // justifyContent: 'flex-start'
+  },
+  row: {
+    display: 'flex',
+    width: 100,
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  avatar: {
+    margin: 6,
+    height: 24,
+    width: 24
+  },
   current: {
     textAlign: 'right',
-    alignSelf: 'flex-end'
+    alignSelf: 'flex-end',
+    '&$message': {
+      borderRadius: '12px+4px+4px+12px'
+    }
   },
-  other: {},
+  other: {
+    '&$message': {
+      borderRadius: '4px+12px+12px+4px'
+    }
+  },
   text: {
     maxWidth: '300px',
     wordWrap: 'break-word'
@@ -43,27 +67,37 @@ const styles = theme => ({
 
 function Message (props) {
   const {message, classes, user} = props
+  console.log('user in message :', user)
+  console.log('message in message :', message)
 
   // to check dynamically if the message is from current user:
   // --------- 1. create a new obj with 1.classes from props
   // --------- 2."message" class for all messages
   // --------- 3."current/other" with check from props
   const rootClasses = classNames(
-    ...classes,
+    // ...classes,
     classes.message,
     (message.user.id === user) ? classes.current : classes.other
   )
-
+  const right = (message.user.id === user)
   return (
-
     <Paper className={rootClasses} elevation={0}>
-      <Typography variant="headline" component="p">
-        <a>{message.createdDate} {user.name}</a>
-      </Typography>
-
-      <Typography className={classes.text} component="p" children={message.content}>
-
-      </Typography>
+      {right &&
+      <div className={classes.messageHeader}>
+        <div className={classes.row}>
+          <Avatar alt={message.user.login} src={message.user.profilePhoto} className={classes.avatar}/>
+          <Typography variant='caption' children={message.user.login}/>
+        </div>
+      </div>
+      }{!right &&
+    <div className={classes.messageHeader}>
+      <div className={classes.row}>
+        <Typography variant="caption" children={message.user.login}/>
+        <Avatar alt={message.user.login} src={message.user.profilePhoto} className={classes.avatar}/>
+      </div>
+    </div>
+    }
+      <Typography className={classes.text} component="p" children={message.content}/>
     </Paper>
 
   )
