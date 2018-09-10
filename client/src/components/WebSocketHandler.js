750a1d257db0f82bb8a7ee9139f81268f21e76f0
korsa719 <qq936500merlin>
2018-09-10 21:55:39 +0300

    eslint

--- client/src/components/WebSocketHandler.js
+++ client/src/components/WebSocketHandler.js
@@ -3,36 +3,33 @@ import { webSocketDialog } from '../actions/ws'
 import connect from 'react-redux/es/connect/connect'
 import { addDialogFromWs, addMessageFromWs, addNotificationFromWs } from '../actions/wsActions'
 
-
-
 class WebSocketHandler extends React.Component {
   componentDidMount () {
     const {addMessage, addNotification, addDialog} = this.props
-    webSocketDialog((message => {
+    webSocketDialog(message => {
       const {dialogId} = this.props.match.params
       switch (message.type) {
         case 'NEW_MESSAGE':
           console.log('dialogId', dialogId)
           if (dialogId) {
             addMessage(message.messageToFront)
-          }else {
+          } else {
             addNotification(message)
           }
-          return
+          break
         case 'NEW_DIALOG' :
           if (this.props.location.pathname.startsWith('/dialogs')) {
             addDialog(message.dialogToFront)
-          }else {
+          } else {
             addNotification(message)
           }
-          return
+          break
         default:
-          return
       }
-    }).bind(this))
+    })
   }
 
-  render(){
+  render () {
     return null
   }
 }
