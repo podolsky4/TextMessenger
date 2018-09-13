56c1230a27b2fec975b1dbda66dd1649fda042f2
korsa719 <qq936500merlin>
2018-09-13 16:30:48 +0300

    remove unused connection

--- client/src/views/Profile/UserProfileInfoCard.js
+++ client/src/views/Profile/UserProfileInfoCard.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import {connect} from 'react-redux'
 import PersonAdd from '@material-ui/icons/PersonAdd'
 import {withStyles} from '@material-ui/core/styles'
 import classnames from 'classnames'
