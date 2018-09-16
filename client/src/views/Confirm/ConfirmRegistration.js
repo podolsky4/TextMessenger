25af3e8fbf9dd5ffc74a3c74fe65512eadbb866c
korsa719 <qq936500merlin>
2018-09-16 17:07:46 +0300

    added view to ConfirmRegistration component

--- client/src/views/Confirm/ConfirmRegistration.js
+++ client/src/views/Confirm/ConfirmRegistration.js
@@ -1,7 +1,34 @@
 import React, { Fragment } from 'react'
 import { Link } from 'react-router-dom'
+import Typography from '@material-ui/core/Typography'
+import Paper from '@material-ui/core/Paper'
+import Button from '@material-ui/core/Button'
+import withStyles from '../../../node_modules/@material-ui/core/styles/withStyles'
 
-export default class ConfirmRegistration extends React.Component {
+const styles = theme => ({
+  paper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '500px',
+    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+    height: '130px',
+    position: 'absolute',
+    margin: 'auto',
+    left: 0,
+    right: 0,
+    bottom: 0,
+    top: 0
+  },
+  submit: {
+    marginTop: theme.spacing.unit * 3
+  },
+  button: {
+    textDecoration : 'none',
+    color: 'white'
+  }
+})
+class ConfirmRegistration extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
@@ -15,12 +42,24 @@ export default class ConfirmRegistration extends React.Component {
       .then(data => this.setState({message: data.message}))
   }
   render () {
+  const {classes} = this.props
     return (
       <Fragment>
-       <h3>{this.state.message}</h3>
+      <Paper className={classes.paper}>
+        <Typography variant="headline">{this.state.message}</Typography>
 
-      <Link to='/feed' >to home page</Link>
+          <Button
+            type="submit"
+            variant="raised"
+            color="primary"
+            className={classes.submit}
+          >
+            <Link className={classes.button} to='/feed' >to home page</Link>
+          </Button>
+
+      </Paper>
       </Fragment>
-    )
+  )
   }
 }
+export default withStyles(styles)(ConfirmRegistration)
