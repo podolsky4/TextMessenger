7c905c9983d8a275ff3bafe67ae73be104db719c
Illya “full-iron” Fefelov <ilyafefelov@gmail.com>
2018-09-21 17:29:04 +0300

    header - names

--- client/src/views/Header/Header.js
+++ client/src/views/Header/Header.js
@@ -18,181 +18,182 @@ import { ClickAwayListener } from '@material-ui/core/umd/material-ui.production.
 import FetchData from '../../actions/serviceAction'
 
 const styles = (theme,) => ({
-  root: {
-    flexGrow: 1,
-  },
-  headingContainer: {
-    display: 'flex',
-    flexGrow: 1,
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    maxWidth: 500,
-    minWidth: 150,
-    justifyContent: 'flex-start'
-  },
-  heading: {
-    marginLeft: theme.spacing.unit,
-    textTransform: 'capitalize',
-  },
-  appBar: {
-    background: '#455A64'
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-    '@media (min-width: 520px)': {
-      display: 'none',
-    }
-  },
-  icon: {
-    margin: theme.spacing.unit
-  },
-  badge: {
-    top: -5,
-    right: -10
-  },
-  typography: {
-    padding: theme.spacing.unit
-  },
-  headerUser: {
-    padding: '1px'
-  },
-  menuIcons: {
-    '@media (max-width: 520px)': {
-      display: 'none',
-    }
-  },
-  drawer: {}
+	root: {
+		flexGrow: 1,
+	},
+	headingContainer: {
+		display: 'flex',
+		flexGrow: 1,
+		flexDirection: 'row',
+		flexWrap: 'wrap',
+		maxWidth: 500,
+		minWidth: 150,
+		justifyContent: 'flex-start'
+	},
+	heading: {
+		marginLeft: theme.spacing.unit,
+		textTransform: 'capitalize',
+	},
+	appBar: {
+		background: '#455A64'
+	},
+	menuButton: {
+		marginLeft: -12,
+		marginRight: 20,
+		'@media (min-width: 520px)': {
+			display: 'none',
+		}
+	},
+	icon: {
+		margin: theme.spacing.unit
+	},
+	badge: {
+		top: -5,
+		right: -10
+	},
+	typography: {
+		padding: theme.spacing.unit
+	},
+	headerUser: {
+		padding: '1px'
+	},
+	menuIcons: {
+		'@media (max-width: 520px)': {
+			display: 'none',
+		}
+	},
+	drawer: {}
 })
 
 class Header extends React.Component {
-  state = {
-    noUser: false,
-    openDrawer: false,
-    profileUser: null,
-    profileUserId: null,
-  }
+	state = {
+		noUser: false,
+		openDrawer: false,
+		profileUser: null,
+		profileUserId: null,
+	}
 
-  toggleDrawer = () => {
-    this.setState({
-      openDrawer: true
-    })
-  }
+	toggleDrawer = () => {
+		this.setState({
+			openDrawer: true
+		})
+	}
 
-  handleClickAway = () => {
-    this.setState({
-      openDrawer: false,
-    })
-  }
+	handleClickAway = () => {
+		this.setState({
+			openDrawer: false,
+		})
+	}
 
-  componentDidMount () {
-    const {match} = this.props
-    let currentProfile = match.params.userId
+	componentDidMount () {
+		const {match} = this.props
+		let currentProfile = match.params.userId
 
-    if (match.params.userId && (this.state.profileUser === null || (this.state.profileUser && this.state.profileUser.id !== +match.params.userId))) {
-      FetchData.get(`/api/users/${currentProfile}`)
-        .then(res => res.json())
-        .then(data => {
-          this.setState({profileUser: data})
-        })
-    }
-  }
+		if (match.params.userId && (this.state.profileUser === null || (this.state.profileUser && this.state.profileUser.id !== +match.params.userId))) {
+			FetchData.get(`/api/users/${currentProfile}`)
+				.then(res => res.json())
+				.then(data => {
+					this.setState({profileUser: data})
+				})
+		}
+	}
 
-  render () {
-    const {classes, user, notification, pageTitle, match} = this.props
-    const {profileUser} = this.state
-    console.log(this.state)
-    console.log(match.params)
-    const showProfileUser = profileUser && match.params.userId && profileUser.id === +match.params.userId
-    console.log(showProfileUser)
+	render () {
+		const {classes, user, notification, pageTitle, match} = this.props
+		const {profileUser} = this.state
+		console.log(this.state)
+		console.log(match.params)
+		const showProfileUser = profileUser && match.params.userId && profileUser.id === +match.params.userId
+		console.log(showProfileUser)
 
-    const locationRender = () => {
-      return pageTitle
-    }
+		const locationRender = () => {
 
-    let {noUser, openDrawer} = this.state
+			return pageTitle
+		}
 
-    if (!user) noUser = true
+		let {noUser, openDrawer} = this.state
 
-    return <div className={classes.root}>
+		if (!user) noUser = true
 
-      <AppBar position='static' className={classes.appBar}>
-        <Toolbar className={classes.toolbar}>
-          <ClickAwayListener onClickAway={this.handleClickAway}>
-            <IconButton className={classes.menuButton}
-                        color="inherit"
-                        aria-label="Menu"
-                        onClick={this.toggleDrawer}>
-              <MenuIcon/>
-            </IconButton>
-            {openDrawer && (<NavMenuDrawer/>)}
-          </ClickAwayListener>
+		return <div className={classes.root}>
 
-          <div className={classes.headingContainer}>
-            {showProfileUser &&
-            <Fragment>
-              <Typography variant="title"
-                          color="inherit"
-                          className={classes.heading}
-                          component={'h3'}
-                          align='justify'
-                          children={profileUser.firstName}
-              />
-              <Typography variant="title"
-                          color="inherit"
-                          className={classes.heading}
-                          component={'h3'}
-                          children={profileUser.lastName}
-              />
-            </Fragment>
-            }
-            {!showProfileUser &&
-            <Typography variant="title"
-                        color="inherit"
-                        className={classes.heading}
-                        component={'h3'}
-                        children={locationRender()}
-            />
-            }
-          </div>
-          <div className={classes.menuIcons}>
-            {/*<IconButton color="inherit" component={Link} to='/'>*/}
-            {/*<HomeIcon className={classes.icon}/>*/}
-            {/*</IconButton>*/}
-            <IconButton color="inherit" component={Link} to='/feed'>
-              <PublicIcon className={classes.icon}/>
-            </IconButton>
-            <IconButton color="inherit" component={Link} to='/favorites'>
-              <FavoriteIcon className={classes.icon}/>
-            </IconButton>
-            <IconButton color="inherit" component={Link} to='/dialogs'>
-              <MessageIcon className={classes.icon}/>
-            </IconButton>
-            <IconButton aria-label="4 pending messages" color="inherit" component={Link}
-                        to='/notifications'>
-              <Badge badgeContent={notification.length} color='secondary'
-                     classes={{badge: classes.badge}}>
-                <NotificationsIcon className={classes.icon}/>
-              </Badge>
-            </IconButton>
-          </div>
-          {!noUser &&
-          <MenuHeader user={user}/>
-          }
-        </Toolbar>
-      </AppBar>
+			<AppBar position='static' className={classes.appBar}>
+				<Toolbar className={classes.toolbar}>
+					<ClickAwayListener onClickAway={this.handleClickAway}>
+						<IconButton className={classes.menuButton}
+												color="inherit"
+												aria-label="Menu"
+												onClick={this.toggleDrawer}>
+							<MenuIcon/>
+						</IconButton>
+						{openDrawer && (<NavMenuDrawer/>)}
+					</ClickAwayListener>
 
-    </div>
-  }
+					<div className={classes.headingContainer}>
+						{showProfileUser &&
+						<Fragment>
+							<Typography variant="title"
+													color="inherit"
+													className={classes.heading}
+													component={'h3'}
+													align='justify'
+													children={profileUser.firstName}
+							/>
+							<Typography variant="title"
+													color="inherit"
+													className={classes.heading}
+													component={'h3'}
+													children={profileUser.lastName}
+							/>
+						</Fragment>
+						}
+						{!showProfileUser &&
+						<Typography variant="title"
+												color="inherit"
+												className={classes.heading}
+												component={'h3'}
+												children={locationRender()}
+						/>
+						}
+					</div>
+					<div className={classes.menuIcons}>
+						{/*<IconButton color="inherit" component={Link} to='/'>*/}
+						{/*<HomeIcon className={classes.icon}/>*/}
+						{/*</IconButton>*/}
+						<IconButton color="inherit" component={Link} to='/feed'>
+							<PublicIcon className={classes.icon}/>
+						</IconButton>
+						<IconButton color="inherit" component={Link} to='/favorites'>
+							<FavoriteIcon className={classes.icon}/>
+						</IconButton>
+						<IconButton color="inherit" component={Link} to='/dialogs'>
+							<MessageIcon className={classes.icon}/>
+						</IconButton>
+						<IconButton aria-label="4 pending messages" color="inherit" component={Link}
+												to='/notifications'>
+							<Badge badgeContent={notification.length} color='secondary'
+										 classes={{badge: classes.badge}}>
+								<NotificationsIcon className={classes.icon}/>
+							</Badge>
+						</IconButton>
+					</div>
+					{!noUser &&
+					<MenuHeader user={user}/>
+					}
+				</Toolbar>
+			</AppBar>
+
+		</div>
+	}
 
 }
 
 const mapStateToProps = state => {
-  return {
-    user: state.user,
-    currentLocation: state.location,
-    notification: state.notification
-  }
+	return {
+		user: state.user,
+		currentLocation: state.location,
+		notification: state.notification
+	}
 }
 
 export default connect(mapStateToProps)(withStyles(styles)(Header))
