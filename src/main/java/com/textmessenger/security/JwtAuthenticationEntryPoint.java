2c72c07f9843ad4a746a2c1b8ef764377782338c
Igor Rudenko <fac30ff@gmail.com>
2018-09-21 10:26:46 +0300

    changed logger in the jwtentrypoint class

--- src/main/java/com/textmessenger/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/textmessenger/security/JwtAuthenticationEntryPoint.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
-  private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
+  private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationEntryPoint.class);
 
   @Override
   public void commence(HttpServletRequest httpServletRequest,
