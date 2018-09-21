241ce01df944fe07b7932b4b7a900bb84e655068
korsa719 <k.matlayev@gmail.com>
2018-09-21 12:50:41 +0300

    Jar (#224)
    * work
    
    * all is working
    jar compiled front & back started
    
    * replace websockettype to entity
    
    * remove from secutiry  "http://localhost:9000/healthcheck"
    
    * replace WTF to front
    
    * FIX PostController, createPost - зачем в маппинге /user?
    
    * FIX PostController, retwitePost - переименовать метод на retweetPost, убрать юзера, спринг дата на пост
    
    * jar

--- src/main/java/com/textmessenger/model/entity/WebSocketType.java
+++ src/main/java/com/textmessenger/model/entity/WebSocketType.java
@@ -1,4 +1,4 @@
-package com.textmessenger.constant;
+package com.textmessenger.model.entity;
 
 public enum WebSocketType {
   NEW_MESSAGE, NEW_POST, NEW_COMMENT, NEW_DIALOG, NEW_RETWEET, NEW_LIKE, NEW_FOLLOWER, ADD_TO_DIALOG
