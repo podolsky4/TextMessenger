60f33c556c252d0d8def1e3f1e75a6885a2cd8bf
korsa719 <k.matlayev@gmail.com>
2018-09-23 20:48:33 +0300

    Member name (#234)
    * fixed members name
    
    * added view to 404

--- client/src/views/404/Page404.js
+++ client/src/views/404/Page404.js
@@ -1,13 +1,50 @@
 import React, {Fragment} from 'react'
 import {Link} from 'react-router-dom'
-
-export default class Page404 extends React.Component {
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import withStyles from '../../../node_modules/@material-ui/core/styles/withStyles'
+const styles = theme => ({
+  paper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '500px',
+    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+    height: '130px',
+    position: 'absolute',
+    margin: 'auto',
+    left: 0,
+    right: 0,
+    bottom: 0,
+    top: 0
+  },
+  submit: {
+    marginTop: theme.spacing.unit * 3
+  },
+  button: {
+    textDecoration : 'none',
+    color: 'white'
+  }
+})
+class Page404 extends React.Component {
   render () {
+    const {classes} = this.props
     return (
       <Fragment>
-        <h1>Sorry page not found</h1>
-        <Link to='/feed' >to home page</Link>
+        <Paper className={classes.paper}>
+          <Typography variant="headline">Sorry page not find</Typography>
+          <Button
+            type="submit"
+            variant="raised"
+            color="primary"
+            className={classes.submit}
+          >
+            <Link className={classes.button} to='/feed' >to home page</Link>
+          </Button>
+        </Paper>
       </Fragment>
     )
   }
 }
+export default withStyles(styles)(Page404)
