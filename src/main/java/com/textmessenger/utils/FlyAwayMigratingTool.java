3031e73eeadd5492a6c89d52f99fc618f473c6ce
Igor Rudenko <fac30ff@gmail.com>
2018-09-25 13:02:34 +0300

    move all sql script to one

--- src/main/java/com/textmessenger/utils/FlyAwayMigratingTool.java
+++ src/main/java/com/textmessenger/utils/FlyAwayMigratingTool.java
@@ -9,6 +9,7 @@ public class FlyAwayMigratingTool {
 
     // Point it to the database
     flyway.setDataSource("jdbc:h2:mem:example-app;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE;MODE=MYSQL;", "sa", null);
+    flyway.baseline();
 
     // Start the migration
     flyway.migrate();
